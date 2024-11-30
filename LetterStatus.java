900e0f6beaca271e3d8c0ff3d5f26f5bc5cd3251
iremyazici20 <75051072+iremyazici20@users.noreply.github.com>
2024-11-30 14:45:42 +0100

    Update LetterStatus.java

--- LetterStatus.java
+++ LetterStatus.java
@@ -1,7 +1,5 @@
 package enums;
 
 public enum LetterStatus {
-    // Harf rengini ayarlamak icin harfin durumunu ayirt eden enum
-    // Olasi durumlar: dogru / dogru ama yanlis yerde / yanlis / verilmemis (devam eden) cevap
     CORRECT, MISPLACED, WRONG, ONGOING_ANSWER
 }
